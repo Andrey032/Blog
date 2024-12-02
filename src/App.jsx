9001423480b1af52e2b2decdcb438041f4e5b73f
Andrey Kabishev <kaadron@yandex.ru>
2024-12-02 17:53:06 +0300

    разворачиваю проект

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { app } from './App.module.css';
+
+function App() {
+  return (
+    <div className={app}>
+      <h2>dfjgsdjgsdgiojsdiogfsdgds</h2>
+      <h1>sdfsdfsdfsdf</h1>
+    </div>
+  );
+}
+
+export default App;
